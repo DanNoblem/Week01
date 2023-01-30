b88817cb893a4b257a76d5885a117bc8934fff98
DanNoble <danielwai999@gmail.com>
2023-01-30 16:22:19 -0500

    Emoji Playground
    First test with emojis

---
+++ Emojis.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+var i = 0
+var emojis = ["😍","🧟‍♂️","🌚","🍑","⛪️"]
+var greeting = "😀"
+print(greeting)
+
+
+for i in 0...4 {
+    var text = "Hello!, \(emojis[i])!"
+    print(text)
+}
