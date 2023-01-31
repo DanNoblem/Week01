4f58afc4a4e274aef6756794bbf5039cf45ba673
DanNoble <danielwai999@gmail.com>
2023-01-31 14:19:16 -0500

    Dicts and random
    More playing around

--- Emojis.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
+++ Emojis.playground/Pages/Page 1.xcplaygroundpage/Contents.swift
@@ -1,11 +1,20 @@
 import UIKit
 var i = 0
-var emojis = ["😍","🧟‍♂️","🌚","🍑","⛪️"]
+var emojis = ["😍","🧟‍♂️","🌚","🍑","⛪️","🍔"]
+var sort = ["food":"🍅","places":"🏦"]
 var greeting = "😀"
+
 print(greeting)
+print(sort)
+print(sort["food",default:"Unknown"])
 
-
-for i in 0...4 {
+for i in 0...5 {
     var text = "Hello!, \(emojis[i])!"
     print(text)
 }
+
+var pick = Int.random(in: 0...emojis.count)
+print(emojis[pick])
+
+
+
